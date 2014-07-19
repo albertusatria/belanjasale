8a20f3b84f05c596073f5a9d81cadf65003a3bd4
albertusatria <albert@icubeonline.com>
2014-07-19 09:34:34 +0700

    pembelian proses

--- application/modules_core/pembelian/controllers/pembelian.php
+++ application/modules_core/pembelian/controllers/pembelian.php
@@ -27,14 +27,52 @@ class Pembelian extends Admin_base {
 		$data['rs_user'] = $this->m_user->get_all_user();
 		// load template
 		$data['message'] = $this->session->flashdata('message');
+		$data['title']		  = "Purchasing Receipt Order Pinaple SI";
+		$data['main_content'] = "menu";
+		$this->load->view('dashboard/admin/template', $data);
+	}
+
+	public function receipt()
+	{
+		// user_auth
+		$this->check_auth('R');
+
+		// menu
+		$data['menu'] = $this->menu();
+		// user detail
+		$data['user'] = $this->user;
+		// get user list
+		$data['rs_user'] = $this->m_user->get_all_user();
+		// load template
+		$data['message'] = $this->session->flashdata('message');
+		$data['title']		  = "Receipt Purchasing Pinaple SI";
+		$data['main_content'] = "receipt";
+		$this->load->view('dashboard/admin/template', $data);
+	}
+
+
+	public function checkin()
+	{
+		// user_auth
+		$this->check_auth('R');
+
+		// menu
+		$data['menu'] = $this->menu();
+		// user detail
+		$data['user'] = $this->user;
+		// get user list
+		$data['rs_user'] = $this->m_user->get_all_user();
+		// load template
+		$data['message'] = $this->session->flashdata('message');
 		$data['title']		  = "POS Inventory Pinaple SI";
 		$data['main_content'] = "pos";
 		$this->load->view('dashboard/admin/template', $data);
 	}
 
+
 	// page title
 	public function page_title() {
-		$data['page_title'] = 'POS Inventory';
+		$data['page_title'] = 'Purchasing';
 		$this->session->set_userdata($data);
 	}
 }
